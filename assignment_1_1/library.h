5f7e17fb504cb77075e0af5cecaaf3dfed5c6e9e
Jonathan Fedje <jonathancf@uia.no>
2024-08-15 14:42:37 +0200

    Ny versjon, fiksa lekse 1.1

--- assignment_1_1/library.h
+++
@@ -1,6 +0,0 @@
-#ifndef ASSIGNMENT_1_1_LIBRARY_H
-#define ASSIGNMENT_1_1_LIBRARY_H
-
-void hello(void);
-
-#endif //ASSIGNMENT_1_1_LIBRARY_H
---
+++ assignment_1_1/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, Christian!\n");
+    return 0;
+}
