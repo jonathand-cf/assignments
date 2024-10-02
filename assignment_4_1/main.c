d8aa57c358d78c4d40706a86b8b8d53db93760f7
Jonathan Fedje <jonathancf@uia.no>
2024-10-02 21:52:44 +0200

    added assignment 4_1

--- assignment_4_1/main.c
+++ assignment_4_1/main.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
+#define CHAR_SIZE 100
+
+char theWord[CHAR_SIZE] = { 0 };
+void is_palindrome(char *theWord)
+{
+    int left = 0;
+    int right = strlen(theWord) - 1;
+    while (left <= right)
+    {
+        if (theWord[left] != theWord[right])
+        {
+            printf("\"%s\" is not palindrome.\n", theWord);
+            return;
+        }
+        left++;
+        right--;
+    }
+    printf("\"%s\" is palindrome.\n", theWord);
+
+}
+void string_reverse(char* theWord)
+{
+    int first = 0;
+    int last = strlen(theWord) - 1;
+    char what;
+    while (first <= last)
+    {
+        what = theWord[first];
+        theWord[first] = theWord[last];
+        theWord[last] = what;
+        first++;
+        last--;
+    }
+}
 int main(void)
 {
-    printf("Testing if changes applies locally if changed here!\n");
+    gets(theWord);
+    size_t length = strlen(theWord);
+    printf("The word contains %zu letters\n", length);
+    is_palindrome(theWord);
+    string_reverse(theWord);
+    printf("The word reversed is '%s'", theWord);
     return 0;
 }
