c2264465b566a95fb6e8fc8150adcf0fd7b557ed
Jonathan Fedje <jonathancf@uia.no>
2024-10-05 18:13:58 +0200

    finnished assignment 4_2

--- assignment_4_2/main.c
+++ assignment_4_2/main.c
@@ -4,47 +4,48 @@
 #define CHAR_SIZE 100
 
 char theWordIs[CHAR_SIZE] = {0};
+char originalWord[CHAR_SIZE] = {0};  // backup for splitte string
+void string_split()
+{
+    int len = strlen(originalWord);
+    int mid = len / 2;  // midten
 
+    printf("The word split in two is '");
+
+    for (int i = 0; i < mid; i++)
+        printf("%c", originalWord[i]);
+    printf(" - ");
+    for (int i = mid; i < len; i++)
+        printf("%c", originalWord[i]);
+
+    printf("'\n");
+}
 void string_upper()
 {
     for (int i = 0; i < CHAR_SIZE; i++)
         theWordIs[i] = (char)toupper(theWordIs[i]);
-    printf("The word in uppercase is '%s'", theWordIs);
+    printf("The word in uppercase is '%s'\n", theWordIs);
 }
 void string_lower()
 {
     for (int i = 0; i < CHAR_SIZE; i++)
         theWordIs[i] = (char)tolower(theWordIs[i]);
-    printf("The word in lowercase is '%s'", theWordIs);
+    printf("The word in lowercase is '%s'\n", theWordIs);
 }
-
 int main(void)
 {
-    int len = 0;
-    do
-    {
-        fgets(theWordIs, CHAR_SIZE, stdin);
-        if (len % 2 != 0)
-        {
-            printf("This program does not support odd number of characters.\n Try again: ");
-        }
-    } while (len % 2 != 0);
+    printf("Enter a word: ");
+    fgets(theWordIs, CHAR_SIZE, stdin);
 
+    // fjerner newline chars hvis d er der (fgets får det med)
+    size_t len = strlen(theWordIs);
+    if (theWordIs[len - 1] == '\n')
+        theWordIs[len - 1] = '\0';
+
+    strcpy(originalWord, theWordIs);
     string_upper(theWordIs);
     string_lower();
-    len = strlen(theWordIs);
-    if (theWordIs[len - 1] == '\n')
-    {
-        theWordIs[len - 1] = '\0';
-        len--;
-    }
-    if (len > 0)
-    {
-        int mid1 = (len / 2) - 1;
-        int mid2 = len / 2;
-        printf("Middle characters: %c - %c\n", theWordIs[mid1], theWordIs[mid2]);
-    }
-    else
-        printf("error");
+    string_split();
+
     return 0;
 }
